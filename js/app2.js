fd895829194d93a4d8b1f7b050ebe2310cc26e79
Fabián Huertas <miloh9318@hotmail.com>
2025-03-20 20:00:13 -0500

    creando funcion crearParrafoTienda

--- js/app2.js
+++ js/app2.js
@@ -1,3 +1,30 @@
+function crearParrafoTienda(textoLabel, valorMin) {
+    // crear etiquetas de parrafo
+    let elementoParrafo = document.createElement('p');
+    
+    //crear etiqueta label
+    let elementoEtiqueta = document.createElement('label');
+    
+    //Conectar con el input
+    elementoEtiqueta.setAttribute("for", textoLabel);
+    
+    //crear etiqueta input
+    let elementoInput = document.createElement('input');
+
+    // Establecer atributos de input
+    elementoInput.setAttribute("type", "number");
+    elementoInput.setAttribute("id", textoLabel);
+    elementoInput.setAttribute("min", valorMin);
+    elementoInput.setAttribute("value", 0);
+
+    //Agregar label e input al parrafo
+    elementoParrafo.appendChild(elementoEtiqueta);
+    elementoParrafo.appendChild(elementoInput);
+
+    //devolver el parrafo completo
+    return elementoParrafo;
+}
+
 function extraerNumeroDesdeElemento(elemento) {
     let miElemento = document.getElementById(elemento);
     let miTexto = miElemento.value;
